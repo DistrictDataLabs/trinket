5f910a5124a4ab70ef6d76725d3f6a52143e7d60
Nathan Danielsen <nathan.danielsen@gmail.com>
2016-06-02 14:27:45 -0700

    update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,20 +8,19 @@ sudo apt-get upgrade -y
 sudo apt-get install -y postgresql-9.5-postgis-2.2 pgadmin3 postgresql-contrib-9.5 postgresql-server-dev-9.5
 sudo apt-get install -y python-dev git python-pip make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev python-virtualenv libsqlite3-dev zip
 
-# setup db and allow django to access it
 cd project/
 
+#set up virtualenv
 virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
+# setup db and allow django to access it
 sudo su
 sudo -u postgres createdb trinket
 sudo -u postgres psql trinket -a -f scripts/db_setup.sql
 sudo cp scripts/pg_hba.conf /etc/postgresql/9.5/main/pg_hba.conf
 service postgresql restart
-
-#set up virtualenv
 
 python manage.py migrate
 
